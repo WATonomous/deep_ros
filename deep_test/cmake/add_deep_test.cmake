5b7a73f7200f68b326f9ff87ff1fde9cb1d63caf
Lucas Reljic-Dumont <56940300+lucasreljic@users.noreply.github.com>
2026-02-03 23:31:36 -0500

    Update Catch2 package requirement in CMake file
    removed version constrait for catch2, should now default to catch2 v3

--- deep_test/cmake/add_deep_test.cmake
+++ deep_test/cmake/add_deep_test.cmake
@@ -32,7 +32,7 @@ function(add_deep_test TEST_NAME TEST_SOURCE)
   )
 
   # Find dependencies
-  find_package(Catch2 2 REQUIRED)
+  find_package(Catch2 REQUIRED)
   find_package(rclcpp REQUIRED)
   find_package(rclcpp_lifecycle REQUIRED)
 
